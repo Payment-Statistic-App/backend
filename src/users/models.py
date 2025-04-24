377a194711d6601b9e0aaa307b27caf7015ba49b
Igor Chesnykh <igor.chesnyx@mail.ru>
2025-04-24 21:24:46 +0300

    Cancel rename

--- src/users/models.py
+++ src/users/models.py
@@ -9,13 +9,10 @@ from sqlalchemy.orm import Mapped, mapped_column
 from src.database import Base
 
 
-class Student(Base):
-    __tablename__ = "students"
+class User(Base):
+    __tablename__ = "users"
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    name: Mapped[str] = mapped_column()
-    surname: Mapped[str] = mapped_column()
-    patronymic: Mapped[str] = mapped_column()
     login: Mapped[str] = mapped_column(unique=True)
     password_hash: Mapped[bytes] = mapped_column()
     created_at: Mapped[datetime.datetime] = mapped_column(default=func.now())
@@ -23,9 +20,6 @@ class Student(Base):
     def to_dict(self) -> Dict[str, Any]:
         return {
             "id": str(self.id),
-            "name": self.name,
-            "surname": self.surname,
-            "patronymic": self.patronymic,
             "login": self.login,
             "created_at": self.created_at.isoformat(),
         }
---
+++ src/users/routers.py
@@ -0,0 +1,38 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends
+
+from src.users.models import User
+from src.users.schemas import UserCreate, Token, UserResponse
+from src.users.services import UserService
+
+router = APIRouter(tags=["user"], prefix="/user")
+
+
+@router.post("/register")
+async def register(user_create: UserCreate) -> Token:
+    user = await UserService().create_user(user_create)
+    access_token = UserService().create_access_token(user)
+    refresh_token = UserService().create_refresh_token(user)
+    return Token(access_token=access_token, refresh_token=refresh_token)
+
+
+@router.post("/login", response_model=Token)
+async def authenticate_user_jwt(user: User = Depends(UserService().authenticate_user)) -> Token:
+    access_token = UserService().create_access_token(user)
+    refresh_token = UserService().create_refresh_token(user)
+    return Token(access_token=access_token, refresh_token=refresh_token)
+
+
+@router.post("/refresh", response_model=Token, response_model_exclude_none=True)
+async def refresh_jwt(
+        user: Annotated[User, Depends(UserService().get_current_user_for_refresh)]
+) -> Token:
+    access_token = UserService().create_access_token(user)
+    return Token(access_token=access_token)
+
+
+@router.get("/self", response_model=UserResponse)
+async def login_for_access_token(
+        current_user: Annotated[User, Depends(UserService().get_current_user)]
+) -> UserResponse:
+    return UserResponse(**current_user.to_dict())
